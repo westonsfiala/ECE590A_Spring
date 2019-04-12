460d655111114ee12759def222df6ec2348e0b30
westonsfiala <westonsfiala@gmail.com>
2019-04-11 19:43:52 -0700

    Added some new files to the project for detecting gestures

--- Assignment2/MazeExplorer/app/src/main/java/com/example/mazeexplorer/MainActivity.kt
+++ Assignment2/MazeExplorer/app/src/main/java/com/example/mazeexplorer/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.mazeexplorer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var listener: OnSwipeTouchListener = OnSwipeTouchListener(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        DisplayText.setOnTouchListener(listener)
+
+        listener.
     }
 }
--- Assignment2/MazeExplorer/app/src/main/java/com/example/mazeexplorer/OnSwipeTouchListener.kt
+++ Assignment2/MazeExplorer/app/src/main/java/com/example/mazeexplorer/OnSwipeTouchListener.kt
@@ -1,12 +1,13 @@
+package com.example.mazeexplorer
 
 import android.content.Context
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
 
-open class OnSwipeTouchListener : View.OnTouchListener {
+open class OnSwipeTouchListener(context: Context) : View.OnTouchListener {
 
-    private val gestureDetector = GestureDetector(GestureListener())
+    private val gestureDetector = GestureDetector(context, GestureListener())
 
     fun onTouch(event: MotionEvent): Boolean {
         return gestureDetector.onTouchEvent(event)
@@ -14,8 +15,8 @@ open class OnSwipeTouchListener : View.OnTouchListener {
 
     private inner class GestureListener : GestureDetector.SimpleOnGestureListener() {
 
-        private val SWIPE_THRESHOLD = 50f
-        private val SWIPE_VELOCITY_THRESHOLD = 100f
+        private val mSwipeThreshold = 50f
+        private val mSwipeVelocityThreshold = 100f
 
         override fun onDown(e: MotionEvent): Boolean {
             return true
@@ -26,14 +27,13 @@ open class OnSwipeTouchListener : View.OnTouchListener {
             return true
         }
 
-
         override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
             val result = false
             try {
                 val diffY = e2.y - e1.y
                 val diffX = e2.x - e1.x
                 if (Math.abs(diffX) > Math.abs(diffY)) {
-                    if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                    if (Math.abs(diffX) > mSwipeThreshold && Math.abs(velocityX) > mSwipeVelocityThreshold) {
                         if (diffX > 0) {
                             onSwipeRight()
                         } else {
@@ -41,7 +41,13 @@ open class OnSwipeTouchListener : View.OnTouchListener {
                         }
                     }
                 } else {
-                    // onTouch(e);
+                    if (Math.abs(diffY) > mSwipeThreshold && Math.abs(velocityY) > mSwipeVelocityThreshold) {
+                        if (diffY > 0) {
+                            onSwipeTop()
+                        } else {
+                            onSwipeBottom()
+                        }
+                    }
                 }
             } catch (exception: Exception) {
                 exception.printStackTrace()
@@ -52,6 +58,7 @@ open class OnSwipeTouchListener : View.OnTouchListener {
     }
 
     override fun onTouch(v: View, event: MotionEvent): Boolean {
+        v.performClick()
         return gestureDetector.onTouchEvent(event)
     }
 
