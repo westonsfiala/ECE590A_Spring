bc38aaed8ee2399edd9c64d0e8f615db815983ed
westonsfiala <westonsfiala@gmail.com>
2019-05-24 21:36:50 -0700

    Figured out how to add a menu to the toolbar

--- Final/CustomDiceRoller/app/src/main/java/com/example/customdiceroller/MainActivity.kt
+++ Final/CustomDiceRoller/app/src/main/java/com/example/customdiceroller/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.customdiceroller
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.customdiceroller.ui.main.SectionsPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,8 +17,33 @@ class MainActivity : AppCompatActivity() {
         view_pager.adapter = SectionsPagerAdapter(this, supportFragmentManager)
         tabs.setupWithViewPager(view_pager)
 
-        toolbar.title = getString(R.string.app_name)
-
         setSupportActionBar(toolbar)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.do_something -> {
+                val text = "do something clicked"
+                val duration = Toast.LENGTH_LONG
+                Toast.makeText(this, text, duration).show()
+                true
+            }
+            R.id.settings_item -> {
+                val text = "settings clicked"
+                val duration = Toast.LENGTH_LONG
+                Toast.makeText(this, text, duration).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
 }
